2f67b5f1ccdb59c62775250a365315e987cf70e2
MeaganAntunes <65407431+MeaganAntunes@users.noreply.github.com>
2020-05-15 16:58:40 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
+def crop_image(img):
+    crop_img = img[0:img.shape[0], 50:img.shape[1]]
+    return crop_img
 
+def normalize(img):
+    min_pixel = np.amin(img)
+    max_pixel = np.amax(img)
+    norm_img = img - min_pixel * 1 / (max_pixel - min_pixel)
+    return norm_img

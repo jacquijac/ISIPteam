f1def0948437aa459ccd33d36d875a878eb9c7ef
jacquijac <65390179+jacquijac@users.noreply.github.com>
2020-05-19 08:48:44 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,13 +6,19 @@ import numpy as np
 
 
 def crop_image(img):
-    crop_img = img[0:img.shape[0], 50:img.shape[1]]
+    '''
+    Crops image to remove text in top left/right corner.
+    '''
+    crop_img = img[50:img.shape[0], 0:img.shape[1]]
     return crop_img
 
 def normalize(img):
+    '''
+    Normalize image so all pixel values are between 0 and 1.
+    '''
     min_pixel = np.amin(img)
     max_pixel = np.amax(img)
-    norm_img = img - min_pixel * 1 / (max_pixel - min_pixel)
+    norm_img = (img - min_pixel) * (1 / (max_pixel - min_pixel))
     return norm_img
 
 

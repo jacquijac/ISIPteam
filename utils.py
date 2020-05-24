253f443990d14a37470d01250887d283365fbb71
Iza410 <65407731+Iza410@users.noreply.github.com>
2020-05-24 23:06:43 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -84,6 +84,25 @@ def create_mask(img):
     mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
     return mask 
 
+def create_circular_mask(h, w, center=None, radius=None): 
+
+    if center is None: # use the middle of the image
+        center = (int(w/2), int(h/2))
+    if radius is None: # use the smallest distance between the center and image walls
+        radius = min(center[0], center[1], w-center[0], h-center[1])
+
+    Y, X = np.ogrid[:h, :w]
+    dist_from_center = np.sqrt((X - center[0])**2 + (Y-center[1])**2)
+
+    mask = dist_from_center <= radius
+    return mask
+#it occurred to me so that maybe it would work better with a mask in the shape of a circle 
+#- then the electrodes do not cut off, and in some images this glowing tail is cut off
+#h, w = img.shape[:2]
+#mask = create_circular_mask(h, w)
+#masked_img = image.copy()
+#masked_img[~mask] = 0
+
 def find_components(mask):
     '''
     Function finds all connected components (white parts of image), then determines which section of the image

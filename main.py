1d946e6cb3dcaab376891243fa1dfd4d4cf707e5
jacquijac <65390179+jacquijac@users.noreply.github.com>
2020-05-31 20:41:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,10 @@
 #import modules
-import sys
-import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import skimage
-from skimage import feature, color, filters, measure
-from scipy import ndimage, spatial
-from PIL import Image
 import cv2
 import utils
-import xlwt 
-from xlwt import Workbook 
-import imutils
 import openpyxl as pyx
 
-#maybe add loop?
-
-# import pre and post images
-#post = plt.imread("DATA/ID03/ID03post.png")
-#pre = plt.imread("DATA/ID03/ID03pre.png")
-#post = plt.imread("DATA/ID04/ID04post.png")
-#pre = plt.imread("DATA/ID04/ID04pre.png")
-#post = plt.imread("DATA/ID05/ID05post.png")
-#pre = plt.imread("DATA/ID05/ID05pre.png")
-#post = plt.imread("DATA/ID06/ID06post.png")
-#pre = plt.imread("DATA/ID06/ID06pre.png")
-#post = plt.imread("DATA/ID07/ID07post.png")
-#pre = plt.imread("DATA/ID07/ID07pre.png")
-#post = plt.imread("DATA/ID14/ID14post.png")
-#pre = plt.imread("DATA/ID14/ID14pre.png")
-#post = plt.imread("DATA/ID15/ID15post.png")
-#pre = plt.imread("DATA/ID15/ID15pre.png")
-#post = plt.imread("DATA/ID17/ID17post.png")
-#pre = plt.imread("DATA/ID17/ID17pre.png")
-#post = plt.imread("DATA/ID18/ID18post.png")
-#pre = plt.imread("DATA/ID18/ID18pre.png")
-#post = plt.imread("DATA/ID37/ID37post.png")
-#pre = plt.imread("DATA/ID37/ID37pre.png")
-#post = plt.imread("DATA/ID38/ID38post.png")
-#pre = plt.imread("DATA/ID38/ID38pre.png")
-post = plt.imread("DATA/ID55/ID55post.png")
-pre = plt.imread("DATA/ID55/ID55pre.png")
-
-
-  
 
 #### Loop to input files ####
 
@@ -72,33 +33,17 @@ for i in ids:
     thresh_img_spiral = utils.select_spiral(thresh_img, center)
     elec, cnts = utils.find_electrodes(post_norm, thresh_img_spiral, center)
 
-    elec_nums = utils.enumerate_electrodes(cnts, center, thresh_img)
+    #ennumerate electrodes 
+    enum_electrodes = utils.enumerate_electrodes(cnts, center, thresh_img)
 
-    for i in range(len(elec_nums)):
-        cv2.putText(post_norm, str(i), (int(elec_nums[i][0]), int(elec_nums[i][1])), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,225), 3)             
+    #plot center and electrode number to post-operative picture
+    enum_electrodes.reverse()
+    for elec in range(len(enum_electrodes)):
+        cv2.putText(post_norm, str(elec+1), (int(enum_electrodes[elec][0]), int(enum_electrodes[elec][1])), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,225), 3)             
     plt.imshow(post_norm)
     plt.show()
     
-    
-    
-    mask = utils.find_bright_points(post_norm)
-    comp_img = utils.find_components(mask)
-    x_center, y_center = utils.find_center(comp_img)
-    center = (x_center, y_center)
-    
-    #plot pre-operative image with red dot at spiral center
-    plt.plot(x_center, y_center, 'r.', markersize=14)
-    plt.imshow(pre_crop)
-    plt.show()
-    
-    #localize electrodes 
-    elec_img, elec_coords = utils.find_electrodes2(post_norm, mask)
-    
-    #ennumerate electrodes 
-    enum_electrodes = utils.enumerate_electrodes(elec_coords, center) 
-    ##depending on format of output of ennumerate function, add plot with electrodes labeled
-    ##
-     #find insertion angle
+    #filter out additional points detected not belonging to 12 piece set of electrodes 
     last=len(enum_electrodes)
     if last>12:
         enum_electrodes=enum_electrodes[:12]
@@ -106,8 +51,9 @@ for i in ids:
     
     #find individual angles between points
     ref = utils.find_insertion_angle(center, enum_electrodes[-1], enum_electrodes[-1])
-    print('For ID' + i + 'angles are:')
+    print('For ID' + str(i) + ' angles are:')
     print(last, 'New angle: {:.2f}'.format(abs(ref)), 'Total insertion angle: {:.2f}'.format(ref))
+    
     #add up angles for insertion depth
     output={last:ref}
     for j in range(len(enum_electrodes)-1):
@@ -117,14 +63,12 @@ for i in ids:
         print(number, 'New angle: {:.2f}'.format(abs(angle)), 'Total insertion angle: {:.2f}'.format(ref))
         output[number]=ref
 
-    #set the output coordinates for every sample
-    #output to excel
+    #set the output coordinates and angles for every sample to print to outputsheet
   
-    # Call a Workbook() function of openpyxl to open result sheet
+    # Call a Workbook() function of openpyxl to open ore nade result sheet
     wb = pyx.load_workbook(filename = 'results_IBMAJA.xlsx')
   
     # Get workbook active sheet   
-    # from the active attribute 
     sheet = wb.active 
   
     #define starting cell for every ID
@@ -162,4 +106,3 @@ for i in ids:
 
     #save in file
     wb.save("results_IBMAJA.xlsx")
-  
